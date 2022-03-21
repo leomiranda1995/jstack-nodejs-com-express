67d490091aeda6872f32bc7d2c0c29d0ef081ac6
Leonardo da Silva Miranda <leomiranda1995@gmail.com>
2022-03-21 00:40:08 -0300

    Refatorando o método delete
    #Remover o pacote uuid
    yarn remove uuid

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,23 +1,4 @@
-const { v4 } = require('uuid');
-
 const db = require('../../database');
-
-let contacts = [
-  {
-    id: v4(),
-    name: 'Leonardo',
-    email: 'leonardo@mail.com',
-    phone: '342352333',
-    category_id: v4(),
-  },
-  {
-    id: v4(),
-    name: 'Jose',
-    email: 'jose@mail.com',
-    phone: '342352668',
-    category_id: v4(),
-  },
-];
 
 class ContactsRepository {
   async findAll(orderBy = 'ASC') {
@@ -67,11 +48,9 @@ class ContactsRepository {
     return row;
   }
 
-  delete(id) {
-    return new Promise((resolve) => {
-      contacts = contacts.filter((contact) => contact.id !== id);
-      resolve();
-    });
+  async delete(id) {
+    const deleteOp = await db.query('DELETE FROM contacts WHERE id = $1', [id]);
+    return deleteOp;
   }
 }
 
